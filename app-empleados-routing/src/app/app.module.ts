14cedeca59de192985be450c24961bb3f31fb588
dalonsogz <dalonsogz@yahoo.es>
2023-06-21 23:14:30 +0200

    CURSO ANGULAR. PROTEGIENDO PÁGINAS. VÍDEO 45

--- app-empleados-routing/src/app/app.module.ts
+++ app-empleados-routing/src/app/app.module.ts
@@ -19,12 +19,13 @@ import { HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { CookieService } from 'ngx-cookie-service';
 import { LoginService } from './login/login.service';
+import { LoginGuardian } from './login/login-guardian';
 
 const appRoutes:Routes=[
   {path:'', component: HomeComponentComponent},
   {path:'proyectos', component: ProyectosComponentComponent},
-  {path:'quienes', component: QuienesComponentComponent},
-  {path:'contacto', component: ContactoComponentComponent},
+  {path:'quienes', component: QuienesComponentComponent, canActivate:[LoginGuardian]},
+  {path:'contacto', component: ContactoComponentComponent, canActivate:[LoginGuardian]},
   {path:'actualiza/:id', component: ActualizaComponentComponent},
   {path:'login', component: LoginComponent},
   {path:'**', component: ErrorPersonalizadoComponent}
@@ -49,7 +50,7 @@ const appRoutes:Routes=[
     RouterModule.forRoot(appRoutes),
     HttpClientModule
   ],
-  providers: [ServicioEmpleadosService, EmpleadosService, DataServices, LoginService, CookieService],
+  providers: [ServicioEmpleadosService, EmpleadosService, DataServices, LoginService, CookieService, LoginGuardian],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
